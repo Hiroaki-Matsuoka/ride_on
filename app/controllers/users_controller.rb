6adb2fbf91ad71d56d78d9071c6237e60b8ebbcd
hiroaki matsuoka <ro09pe05.kauru@gmail.com>
2020-07-26 21:53:43 +0900

    make_users_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,15 +13,25 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to current_user
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました"
+      redirect_to current_user
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :image, :image_name)
   end
 
 end
