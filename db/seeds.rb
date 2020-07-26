8dd52697531157038e72a17de0c93dd4d3e3516b
hiroaki matsuoka <ro09pe05.kauru@gmail.com>
2020-07-26 09:21:45 +0900

    add_active_admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 Ride.create(title: "アワイチライド", message: "みんなで淡路島を一周しよう！", address: "大阪府和泉市", user_id: 1)
 Ride.create(title: "ビワイチライド", message: "みんなで琵琶湖を一周しよう！", address: "大阪府和泉市", user_id: 1)
 Ride.create(title: "高野山ライド", message: "みんなで高野山へいこう！", address: "大阪府和泉市", user_id: 1)
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
