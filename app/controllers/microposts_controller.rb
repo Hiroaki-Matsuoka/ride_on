1b03d1e9fa27564a18bfacadafad707d9d378bae
hiroaki matsuoka <ro09pe05.kauru@gmail.com>
2020-07-29 09:11:13 +0900

    add_flash

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,8 +4,8 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      # flash[success] = "Micropost created!"
-      redirect_to root_url
+      flash[:notice] = "Micropost created!"
+      redirect_to user_path(current_user.id)
     else
       redirect_to root_url
     end
