b4e2403c47839cb05c8b7b7d2f2498deff037cea
hiroaki matsuoka <ro09pe05.kauru@gmail.com>
2020-08-07 08:19:03 +0900

    ゲストユーザー追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :rides
   has_many :joins
-  has_many :j_rides, through: :joins, source: :ride 
+  has_many :j_rides, through: :joins, source: :ride
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
                                 foreign_key: "follower_id",
@@ -42,6 +42,12 @@ class User < ApplicationRecord
   def unjoin(ride)
     join = joins.find_by(ride_id: ride.id)
     join.destroy if join
+  end
+
+  def self.guest
+    find_or_create_by!(name: 'guestさん', email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   get "joins/index" => "joins#index"
   post "joins/:ride_id/create" => "joins#create"
   post "joins/:ride_id/destroy" => "joins#destroy"
   root to: 'staticpages#home'
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :rides do
     resources :joins
   end
