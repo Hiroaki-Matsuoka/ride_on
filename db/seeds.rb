67b1c5be234ab4e07b0239a6cea8cba3e3aa88cf
hiroaki matsuoka <ro09pe05.kauru@gmail.com>
2020-08-19 09:50:00 +0900

    TOPページレスポンシブ対応

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,6 @@ Ride.create(title: "アワイチライド", message: "みんなで淡路島を�
 Ride.create(title: "ビワイチライド", message: "みんなで琵琶湖を一周しよう！", address: "大阪府和泉市", user_id: 1)
 Ride.create(title: "高野山ライド", message: "みんなで高野山へいこう！", address: "大阪府和泉市", user_id: 1)
 
-# User.create!(name: "Hiro",
-#             email: "test@example.com",
-#             password: "password",
-#             password_confirmation: "password",
-#             image_name: "default.jpeg")
-
 users= [
 { name: 'Hiiro', email: 'tes@example.com', password: 'password', image_name: 'default.jpeg' },
 { name: 'Aki', email: 'hoge@example.com', password: 'password', image_name: 'default.jpeg' },
@@ -19,16 +13,3 @@ users= [
 users.each do |record|
  User.create!(record) unless User.find_by(email: record[:email])
 end
-
-
-
-# 99.times do |n|
-#   name  = Faker::Name.name
-#   email = "example-#{n+1}@example.com"
-#   password = "password"
-#   User.create!(name:  name,
-#                email: email,
-#                password:              password,
-#                password_confirmation: password,
-#                image_name: "default.jpeg")
-# end
