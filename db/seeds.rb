089dc22fba8d717d370587d53ff637ea162ef2a6
hiroaki matsuoka <ro09pe05.kauru@gmail.com>
2020-08-19 16:35:21 +0900

    seedデータ変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,19 @@
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-Ride.create(title: "アワイチライド", message: "みんなで淡路島を一周しよう！", address: "大阪府和泉市", user_id: 1)
-Ride.create(title: "ビワイチライド", message: "みんなで琵琶湖を一周しよう！", address: "大阪府和泉市", user_id: 1)
-Ride.create(title: "高野山ライド", message: "みんなで高野山へいこう！", address: "大阪府和泉市", user_id: 1)
-
 users= [
-{ name: 'Hiiro', email: 'tes@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'Hiro', email: 'test@example.com', password: 'password', image_name: 'default.jpeg' },
 { name: 'Aki', email: 'hoge@example.com', password: 'password', image_name: 'default.jpeg' },
 { name: 'Jakub', email: 'foo@example.com', password: 'password', image_name: 'default.jpeg' },
-{ name: 'miyo', email: 'bar@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'you', email: 'bar@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'まつおか', email: 'ba@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'ひろあき', email: 'br@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'foo', email: 'b@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'bar', email: 'ar@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'hoge', email: 'fo@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'hogehoge', email: 'hg@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'foobar', email: 'testes@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'for', email: 'testes99@example.com', password: 'password', image_name: 'default.jpeg' },
+{ name: 'fobr', email: 'testes100@example.com', password: 'password', image_name: 'default.jpeg' },
 ]
 users.each do |record|
  User.create!(record) unless User.find_by(email: record[:email])
