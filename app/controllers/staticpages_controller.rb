07f4d70314438f930760d7a666c3c56804fa46d7
hiroaki matsuoka <ro09pe05.kauru@gmail.com>
2020-08-29 13:31:00 +0900

    マイページリンク縮小

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -2,8 +2,6 @@ class StaticpagesController < ApplicationController
 
   def home
     if user_signed_in?
-      # @micropost = current_user.microposts.build
-      # @feed_items = current_user.feed.paginate(page: params[:page])
       @q = Ride.ransack(params[:q])
       @rides = @q.result(distinct: true)
       @user = User.find(current_user.id)
