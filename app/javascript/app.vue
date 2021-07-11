f58efd6f70d6d257b09342a9c96ddeaddd031f42
hiroaki matsuoka <matsuoka@skygrid.co.jp>
2021-07-11 13:55:47 +0900

    testに変更

---
+++ app/javascript/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <p>{{ test }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "テスト",
+      test: "あああああ"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
